1c4f03abe3086ef9d509439cee77ce9cfc54ed91
Dušan Andrić <Dusan.Andric@gmail.com>
2024-12-02 16:18:12 -0500

    add todos

--- cmd/greeting.go
+++ cmd/greeting.go
@@ -12,13 +12,8 @@ import (
 // greetingCmd represents the greeting command
 var greetingCmd = &cobra.Command{
 	Use:   "greeting",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "TODO: Add a short description here",               // TODO
+	Long:  `TODO: Add a longer description here or remove it`, // TODO
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("greeting called")
 	},
